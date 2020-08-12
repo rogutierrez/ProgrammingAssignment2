a0c9ed8b551dfc340f9abaa39e63ddfb9cacbee6
Rodrigo Gutiérrez Martínez <rogutierrezmartinez@gmail.com>
2020-08-12 01:37:02 -0400

    resolucion terminada

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Las siguientes funciones almacenan cálculos en la memoria caché de forma que se pueden
+## utilizar en cálculos posteriores.
 
-## Write a short comment describing this function
+## La función crea una lista para estableser (set) o obtener (get) una matriz y su inversa.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(
+    set = set, get = get,
+    setsolve = setsolve,
+    getsolve = getsolve
+  )
 }
 
 
-## Write a short comment describing this function
+## Esta función calcula la inversa de una matriz cuadrada si es que no está el cálculo en
+## la caché. Si el cálculo está, simplemente lo utiliza sin realizar cálculos extra.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
